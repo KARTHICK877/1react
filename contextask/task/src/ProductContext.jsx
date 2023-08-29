3a0712a17e88ccdf31ba28dfd7464d26c57bb80c
KARTHICK877 <kmass8754@gmail.com>
2023-08-29 17:16:55 +0530

    react

--- contextask/task/src/ProductContext.jsx
+++ contextask/task/src/ProductContext.jsx
@@ -1,6 +1,31 @@
-// ProductContext.js
-import { createContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const ProductContext = createContext();
 
-export default ProductContext;
+export const useProductContext = () => {
+  return useContext(ProductContext);
+};
+
+export function ProductProvider({ children }) {
+  const [cartCount, setCartCount] = useState(0);
+
+  const addToCart = () => {
+    setCartCount(cartCount + 1);
+  };
+
+  const removeFromCart = () => {
+    setCartCount(cartCount - 1);
+  };
+
+  const initialState = {
+    cartCount,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
+    <ProductContext.Provider value={initialState}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
